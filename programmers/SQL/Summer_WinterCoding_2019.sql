9a508877db0caeaf24f05e779a974328e7b50aa3
이선우 <mercyrain23@gmail.com>
2021-02-19 21:26:24 +0900

    commit

---
+++ programmers/SQL/Summer_WinterCoding_2019.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT(CART_ID)
+FROM CART_PRODUCTS
+WHERE CART_ID IN (SELECT
+                    CART_ID
+                FROM CART_PRODUCTS
+                WHERE NAME = "MILK")
+AND NAME="Yogurt"
